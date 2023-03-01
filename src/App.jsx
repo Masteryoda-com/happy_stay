ebb05518d34fe99d4bd1ef3623372d8d99532bd3
Mohan Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-03-01 11:36:22 +0530

    chore : added some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/molecules/Navbar/Navbar'
+import Home from './components/templates/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>happy stay</h1>
+    <Navbar/>
+    <Home/>
     </>
   )
 }
